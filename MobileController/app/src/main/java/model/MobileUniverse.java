dfae03a637647dc60c88e8b67f39983cc12410bd
Shao Jie Hu Chen <54322143+SH02-tech@users.noreply.github.com>
2023-03-01 12:58:53 +0100

    Skeleton for model interface

--- MobileController/app/src/main/java/model/MobileUniverse.java
+++ MobileController/app/src/main/java/model/MobileUniverse.java
@@ -1,54 +1,5 @@
 package model;
 
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
-
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-
 public class MobileUniverse {
-    private ControlDroid controlDroid;
-    private ArduSocket arduSocket;
 
-    public MobileUniverse() {
-        controlDroid = null;
-        arduSocket = null;
-    }
-
-    public MobileUniverse(UsbManager usbManager, String host, int port) {
-        setControlDroid(usbManager);
-        setArduSocket(host, port);
-    }
-
-    public void setArduSocket(String host, int port) {
-        arduSocket = new ArduSocket(host, port);
-    }
-
-    public void setControlDroid(UsbManager usbManager) {
-        controlDroid = new ControlDroid(usbManager);
-    }
-
-    public UsbDevice findArduinoDevice() {
-        return controlDroid.findDevice();
-    }
-
-    public boolean connectArduinoDevice(UsbDevice usbDevice) {
-        return controlDroid.connectDevice(usbDevice);
-    }
-
-    public void init() {
-        String msg;
-
-        while (true) {
-
-            msg = arduSocket.receivedMessage();
-
-            while (msg == ArduSocket.NULLMESSAGE) {
-                msg = arduSocket.receivedMessage();
-            }
-
-            controlDroid.sendOrder("f");
-        }
-    }
 }
