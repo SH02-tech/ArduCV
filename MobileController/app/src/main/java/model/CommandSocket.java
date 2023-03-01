4886d1ec8eda33a296569815f8cd5f385cbb6454
Shao Jie Hu Chen <54322143+SH02-tech@users.noreply.github.com>
2023-03-01 13:20:15 +0100

    Added documentation.

--- MobileController/app/src/main/java/model/CommandSocket.java
+++ MobileController/app/src/main/java/model/CommandSocket.java
@@ -4,37 +4,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+
 public class CommandSocket {
 
-    private final String DEFAULT_HOST = "127.0.0.1"; // localhost
-    private final int DEFAULT_PORT = 1010;
-
-    String host;
-    int port;
     Socket socket;
     BufferedReader in;
 
+    /**
+     * @brief Constructor of Command Socket. No socket is assigned.
+     */
     public CommandSocket() {
-        super();
-        host = DEFAULT_HOST;
-        port = DEFAULT_PORT;
         socket = null;
         in = null;
     }
 
-    public void setHost(String the_host) {
-        this.host = the_host;
-    }
-
-    public void setPort(int the_port) {
-        this.port = the_port;
-    }
-
-    public boolean connect(String the_host, int the_port) {
+    /**
+     * @brief Establish a connection to a socket.
+     * @param host IP of the host of socket.
+     * @param port Port of the socket.
+     * @return True if connection is established successfully. False otherwise.
+     */
+    public boolean connect(String host, int port) {
         boolean status = true;
-
-        setHost(the_host);
-        setPort(the_port);
 
         try {
             socket = new Socket(host, port);
@@ -46,6 +37,10 @@ public class CommandSocket {
         return status;
     }
 
+    /**
+     * @brief Gets message from socket. It reads entire lines, separated by newlines.
+     * @return String of message. In case of IO error, empty string is returned.
+     */
     public String getMessage() {
         String data = null;
 
@@ -58,6 +53,10 @@ public class CommandSocket {
         return data;
     }
 
+    /**
+     * @brief Return the status of socket.
+     * @return True if connection is closed. False otherwise.
+     */
     public boolean isClosed() {
         return socket.isClosed();
     }
